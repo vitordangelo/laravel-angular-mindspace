5104e7a07d8007efc7c920d3cd0a053e44f9b3b1
Vitor <metroid.p2p@gmail.com>
2017-05-07 10:26:20 -0300

    first commit

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    public function index()
+    {
+        $quotes = Quote::all();
+
+        if(!$quotes) 
+        {
+            return response()->json(['error' => 'Document not found'], 404);
+        }
+
+        return response()->json([ 'status' => 'Success', 'data' => $quotes], 200);
+    }
+
+    public function store(Request $request)
+    {
+        // $quote = Quote::create($request->all());
+        $quote = new Quote();
+        $quote->content = $request->input('content');
+        $quote->save();
+
+        if(!$quote->content) 
+        {
+            return response()->json(['error' => 'Não pode ser vazio'], 404);
+        }
+
+        return response()->json([ 'status' => 200, 'data' => $quote], 200);
+    }
+
+    public function show(Quote $quote)
+    {
+        $quote = Quote::find($id);
+
+        if (!$quote)
+        {
+            return response()->json(['error' => 'Document not found'], 404);
+        }
+
+        return response()->json([ 'status' => 200, 'data' => $quote], 200);
+    }
+
+    public function update(Request $request, Quote $quote)
+    {
+        $quote = Quote::find($id);
+
+        if (!$quote) {
+            return response()->json(['error' => 'Document not found'], 404);
+        }
+
+        $quote->update($request->all());
+
+        return response()->json([ 'status' => 200, 'data' => $quote], 200);
+    }
+
+    public function destroy(Quote $quote)
+    {
+        $quote = Quote::destroy($id);
+
+        if (!$quote) {
+            return response()->json(['error' => 'Document not found'], 404);
+        }
+
+        $quote->update($request->all());
+
+        return response()->json([ 'status' => 200, 'data' => $quote], 200);
+    }
+}
